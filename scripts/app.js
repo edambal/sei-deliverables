94adca2f98b44a940b138fe68c0ce7a998cc11b7
edambal <bal818edam@gmail.com>
2020-09-12 08:38:37 -0700

    Commit 2 - Get Even answered

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,15 @@ for(let i=1;i<=20;i++){
     console.log(i);
 }
 
+
+// ## Get Even
+// 1. Write a `for` loop that will log only the even numbers in 0 through 200.
+// >Hint: Think about the increment expression.
+
+// <hr>
+// &#x1F534; The commit message should read: <br>
+// "Commit 2 - Get Even answered"
+
+for(let i=0;i<=200;i+=2){
+    console.log(i);
+}
